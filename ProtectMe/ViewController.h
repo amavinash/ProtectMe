5df5be2f01c02b7d3fa71d9b6901859fab4ee78f
Avinash Bhandarkar <avinashb123@gmail.com>
2016-01-06 19:35:46 +0530

    Initial Commit

---
+++ ProtectMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtectMe
+//
+//  Created by Avinash Bhandarkar on 1/6/16.
+//  Copyright © 2016 Avinash Bhandarkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProtectMe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProtectMe
+//
+//  Created by Avinash Bhandarkar on 1/6/16.
+//  Copyright © 2016 Avinash Bhandarkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
